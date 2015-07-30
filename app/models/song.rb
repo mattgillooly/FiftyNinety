281043e4e51d28b4f8843d62859804fabbd3b50d
Matt Gillooly <git@mattgillooly.com>
2015-07-30 01:31:19 -0400

    move Song remote link building to model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,13 @@
+require 'uri'
+
 class Song < ActiveRecord::Base
+
+  def remote_url
+    URI.join('http://fiftyninety.fawmers.org/song/', remote_id.to_s).to_s
+  end
+
+  def remote_user_url
+    URI.join('http://fiftyninety.fawmers.org/', user_href).to_s
+  end
+
 end

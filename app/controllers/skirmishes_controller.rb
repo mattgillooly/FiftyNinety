81927b4545bf588669841ea20eb91646c528130c
Matt Gillooly <git@mattgillooly.com>
2015-08-05 13:53:57 -0400

    build ICS feed as UTC events
    note times must be suppplied as strings, or local timezone will be
    assumed.
    https://github.com/icalendar/icalendar/issues/109#issuecomment-118882819

--- app/controllers/skirmishes_controller.rb
+++ app/controllers/skirmishes_controller.rb
@@ -1,7 +1,5 @@
 class SkirmishesController < ApplicationController
 
-  DEFAULT_ZONE = ActiveSupport::TimeZone["America/New_York"]
-
   # GET /skirmishes
   # GET /skirmishes.ics
   def index
@@ -32,32 +30,12 @@ class SkirmishesController < ApplicationController
         @skirmishes.each do |skirmish|
           if skirmish.starts_at
             calendar.event do |e|
-              e.dtstart = skirmish.starts_at
-              e.dtend = skirmish.starts_at + 1.hour
+              e.dtstart = skirmish.starts_at.strftime('%Y%m%dT%H%M%SZ')
+              e.dtend = (skirmish.starts_at + 1.hour).strftime('%Y%m%dT%H%M%SZ')
               e.summary = '50/90 Skirmish'
               e.description = skirmish.url
               e.url = skirmish.url
             end
-          end
-        end
-
-        calendar.timezone do |c|
-          c.tzid = "America/New_York"
-
-          c.daylight do |d|
-            d.tzoffsetfrom =  "-0500"
-            d.tzoffsetto = "-0400"
-            d.tzname = "EDT"
-            d.dtstart = "19700308T020000"
-            d.rrule = "FREQ=YEARLY;BYMONTH=3;BYDAY=2SU"
-          end
-
-          c.standard do |s|
-            s.tzoffsetfrom = "-0400"
-            s.tzoffsetto = "-0500"
-            s.tzname = "EST"
-            s.dtstart = "19701101T020000"
-            s.rrule = "FREQ=YEARLY;BYMONTH=11;BYDAY=1SU"
           end
         end
 

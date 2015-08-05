bafddd74e86db108eea5e394ec44d973c68ac5e7
Matt Gillooly <git@mattgillooly.com>
2015-08-05 14:22:18 -0400

    include 'zoned' start hours for skirmishes in web UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,21 @@ module ApplicationHelper
     end
   end
 
+  ZONES = {
+    'EDT' => ActiveSupport::TimeZone["Eastern Time (US & Canada)"],
+    'PDT' => ActiveSupport::TimeZone["Pacific Time (US & Canada)"],
+    'GMT' => ActiveSupport::TimeZone["London"],
+  }
+
+  def skirmish_link(skirmish)
+    t = skirmish.starts_at
+
+    zoned = ZONES.map do |name, tz|
+      h = tz.utc_to_local(t).hour
+      "#{((h - 1) % 12) + 1}#{ (h > 11 && h < 24) ? 'PM' : 'AM'}&nbsp;#{name}"
+    end.join(' / ').html_safe
+
+    link_to zoned, skirmish.url
+  end
+
 end
